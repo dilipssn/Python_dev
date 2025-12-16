4d0affb4cbedc5dd46405dc12dd7ec07b03dd38e
Swadhikar Chandramohan <cswadhikar@gmail.com>
2025-12-16 17:33:10 +0530

    Review comments for 2nd set

--- 04_lists/04_list_practise-02.py
+++ 04_lists/04_list_practise-02.py
@@ -8,18 +8,39 @@ cart = [29.99, 15.50, 8.99, 45.00]
 # 4. Remove the cheapest item
 # 5. Count how many items cost more than 20
 
-def shopping_cart():
-    cart = [29.99, 15.50, 8.99, 45.00]
+def shopping_cart():    # try to add docstring. I am adding now.
+    """
+        The function performs shopping cart operations as follows:
+        
+        a. Calculates total prices and display
+        b. Applies discount for products cost more than 50
+        c. Adds an item to the cart
+        d. Removes the minimum value cart
+        e. Counts the pricey products greater than 20 
+    """
+    
+    cart = [29.99, 15.50, 8.99, 45.00]  
     total_price = sum(cart)
-    print (f'Total Price in the cart is :{total_price}')
+    print (f'Total Price in the cart is :{total_price}')    # Please add a space between two different operations to differentiate
+    
     if total_price > 50:
         discounted_cart_price = (total_price - (total_price * 0.1)) // 1
         print (f"discounted_cart_price  :{discounted_cart_price}")
 
+    # add item
     cart.append(12.99)
-    print(f"Added a new price Rs. 12.99 into the cart and the new cart is : {cart}")
-    cart.remove(min(cart))
+    print(f"Added a new price Rs. 12.99 into the cart and the new cart is : {cart}")    
+    
+    # remove item
+    cart.remove(min(cart))      # Although single line performs the required operation, it is better to split into two lines as below.
+    
+    """
+    low_cost_product = min(cart)
+    cart.remove(low_cost_product)
+    """
     print(f"Removed a minimum price from the cart and the new cart is : {cart}")
+    
+    # <line break>
     count = 0
     for price in cart:
         if price > 20:
@@ -28,6 +49,7 @@ def shopping_cart():
     print(f"There are {count} items cost more than Rs. 20 in the cart")
 
 shopping_cart()
+
 # outputs are as below:
 # -------------------------
 # Total Price in the cart is :99.48
@@ -55,7 +77,7 @@ def find_phone_by_name(cart: list):
     iphone_gadgets = []
     # loop the products list to search for iPhone
     for phone in cart:
-
+         
     # search for iPhone from the item
         if 'iPhone' in phone:
             iphone_gadgets.append(phone)
@@ -63,15 +85,14 @@ def find_phone_by_name(cart: list):
     # Return the found iPhone  
     return iphone_gadgets
 
-print (find_phone_by_name(gadgets))
+print (find_phone_by_name(gadgets)) # good to segregate into 2 lines.
 
 # Output
 # ['iPhone 15', 'iPhone 14']
 
 # 2. Check if "iPad Pro" is available
 
-
-def check_product(products, search_product):
+def check_product(products, search_product):        # Looks good. This is how method signature should be
     for product in products:
         if product.lower() == search_product.lower():
             return True # Here the loop breaks
@@ -80,19 +101,19 @@ def check_product(products, search_product):
     # all the iterations are exhausted for the products
     return False
 
-print (search_product(gadgets, 'iPod Pro'))
+print (search_product(gadgets, 'iPod Pro'))         # use f strings: print(f"{search_product(gadgets, 'iPod Pro') = }")
 # True
 
 # 3. Count total products
-print(f"The total products in the cart :{len(gadgets)}")
+print(f"The total products in the cart :{len(gadgets)}")    # Simple and good 
  
 # The total products in the cart :5
 
 # 4. Show products in alphabetical order
-def sort_gadgets_alphabetic (gadgets):
-    # Empty list to occumalate gadget in lower case
-    gadgets_lower = []
-
+def sort_gadgets_alphabetic (gadgets):  # review: As I said this function is not required. 
+    gadgets_lower = []                  # print(list.sort()) will yield the desired result: 
+                                        # Sorting doesnt require lower case conversion. 
+                                        # Only string comparison requires lower case conversion
     # loop the gadgets to get each gadget in string, lower() is a string func
     for gadget in gadgets:
         gadgets_lower.append(gadget.lower())
@@ -112,32 +133,33 @@ stock = [5, 0, 12, 3, 0, 8]
 # Tasks:
 # 1. Find how many products are out of stock (0 quantity)
 
- def find_zero_stock(stuffs: list):
-     count = 0
+def find_zero_stock(stuffs: list):
+    count = 0
+                        # review: this space makes code cleaner
+    for item in stuffs:
+                        # review: this space is not required. 
+        if item == 0:
+            count += 1
 
-     for item in stuffs:
+    print (count, 'products are out of stocks now') # use f strings as like below for meaningful information
+    print(f"There are '{count}' products out of stock. Please order new stocks.")
 
-         if item == 0:
-             count += 1
-
-     print (count, 'products are out of stocks now')
-
- find_zero_stock(stock)
+find_zero_stock(stock)
 
 # (2, 'products are out of stocks now')
 
 # 2. Find products with low stock (less than 5)
 def find_low_stock(stuffs: list):
-    count = 0
+    count = 0               # space required
     for item in stuffs:
-        
+                            # space not required
         if item < 5:
             count += 1
 
     print (f"There are {count} products are at low stock range now")
 
-stock = [5, 0, 12, 3, 0, 8]
-find_low_stock(stock)
+stock = [5, 0, 12, 3, 0, 8] 
+find_low_stock(stock)           # this is good. defining array above and invoking below. We can use any no. of stocks to test this way
 
 # There are 3 products are at low stock range now
 
@@ -147,8 +169,15 @@ print (f"total items in the warehouse are : {len(stock)}")
 # total items in the warehouse are : 6
 
 # 4. Check if all products are in stock
-def check_stocks(stocks):
-    """The function checks if all the stocks are available"""
+def check_stocks(stocks):                                       # Looks good. Need to add argument info and return statement as below when a function accepts arg. We will use this in future sessions.
+    """
+        The function checks if all the stocks are available
+        
+        @param  stocks      List of stocks to be validated
+        
+        @return     True    If all stocks are available
+                    False   If any stock is empty   
+    """
     # loop through each of the stock
     for stock in stocks:
         # if any stock has 0 value return False
